4154c1b6de33fb12f5338d31bbd868e067b1ba66
mysterystarfish <paulhsu1016@gmail.com>
2024-05-19 02:06:06 +0800

    Monster add color

--- GarenaGameJam2024/Assets/Scripts/Monster/Monster.cs
+++ GarenaGameJam2024/Assets/Scripts/Monster/Monster.cs
@@ -6,19 +6,26 @@ using UnityEditor;
 
 public class Monster : MonoBehaviour
 {
-#if UNITY_EDITOR
-    [SerializeField] bool UseConstantElementType;
-#endif
     public Element.ElementType elementType;
     public Stat stat;
     public Action<Monster> OnDeadEvent = delegate { };
     private void Awake()
     {
-#if UNITY_EDITOR
-        if (UseConstantElementType) return;
-#endif
         elementType = (ElementType)UnityEngine.Random.Range(0, Enum.GetNames(typeof(ElementType)).Length);
         Debug.Log(elementType);
+        var sprite = GetComponent<SpriteRenderer>();
+        switch (elementType)
+        {
+            case ElementType.Fire:
+                sprite.color = new Color(1, 0, 0, 1);
+                break;
+            case ElementType.Water:
+                sprite.color = new Color(0, 0, 1, 1);
+                break;
+            case ElementType.Wood:
+                sprite.color = new Color(0, 1, 0, 1);
+                break;
+        }
     }
     [Button]
     public void TakeDamage(float damage)
